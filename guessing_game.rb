7169981e36963dc397e1a31693cb279f320c4e66
Marcin <mbaranowski57@gmail.com>
2022-10-11 22:58:29 +0200

    added the results save option

--- guessing_game.rb
+++ guessing_game.rb
@@ -8,43 +8,88 @@
 # Punkt 7 - not done
 # Punkt 8 - not done
 
+$results = Hash.new
+
+def saveResults(names, counter)
+    $results[names] = counter
+end
+
+def printResults()
+    $results.each do |key, value|
+        puts "Gracz #{key} odgadł prawidłową liczbe w #{value} krokach"
+    end
+end
+
+
+def menu()
+    puts "Witamy w Guessing Game"
+    puts "1. Zagraj"
+    puts "2. Zobacz ostatnie wyniki"
+
+    option = gets
+
+    case option.to_i
+    when 1
+        game()
+    when 2
+        printResults()
+        menu()
+    else
+        puts "Nie ma takiej opcji"    
+    end
+end
+
 
 def game()
     target = rand(1..100)
     game_over = false
+    counter = 0    
+    
+    puts "Teraz będziesz zgadywał liczbe"
+        while !game_over
+            puts "Podaj liczbe"
+            input = gets
+            counter += 1
 
-    while !game_over
-        puts "Podaj liczbe"
-        input = gets
-        if input == "koniec\n"
-            puts "żegnaj"
-            break
-        elsif input.to_i > target
-            puts "za duża"
-        elsif input.to_i < target
-            puts "za mała"
-        else
-            puts "Brawo zgadłeś"
-            game_over = true
+            if input == "koniec\n"
+                puts "żegnaj"
+                exit
+            elsif input.to_i > target
+                puts "za duża"
+            elsif input.to_i < target
+                puts "za mała"
+            else
+                puts "Brawo zgadłeś"
+                game_over = true
+            end
         end
-    end
+    
+    puts "Podaj swoje imie i nazwisko, abyśmy mogli zapisać twój wynik"
+    names = gets
+
+    saveResults(names, counter)
+
+    playAgain()
 end
 
-puts "Teraz będziesz zgadywał liczbe"
-game()
 
-end_program = false
-while !end_program
-    puts "Czy gramy jeszcze raz? (Y/N)"
+def playAgain()
+    end_program = false
+    while !end_program
+        puts "Czy gramy jeszcze raz? (Y/N)"
 
-    choice = gets
+        choice = gets
 
-    if choice == "Y\n"
-        game()
-    elsif choice =="N\n"
-        puts "Koniec działania programu"
-        end_program = true
-    else
-        puts "Nie ma takiej opcji mordo"
-    end
+        if choice.upcase == "Y\n"
+            menu()
+        elsif choice.upcase =="N\n"
+            puts "Koniec działania programu"
+            end_program = true
+        else
+            puts "Nie ma takiej opcji mordo"
+        end    
+    end 
 end
+    
+
+menu()
